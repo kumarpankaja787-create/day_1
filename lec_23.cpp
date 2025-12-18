f54615ab6d72684c2a1170677984e7aa97b44173
Nitin Kumar <nitinkumargupta613@gmail.com>
2025-12-18 17:19:49 +0530

    input & output of 2d array

---
+++ lec_23.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    
+    // Input dimensions
+    cout << "Enter number of rows: ";
+    cin >> rows;
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    
+    // Create 2D array
+    int arr[rows][cols];
+    
+    // Input array elements
+    cout << "Enter array elements:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    // Output array elements
+    cout << "\nArray:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
