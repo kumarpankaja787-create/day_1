73b5db9c9502d12fd9a6231b9fa15046e04aa751
Nitin Kumar <nitinkumargupta613@gmail.com>
2025-12-18 23:00:17 +0530

    binary search

--- binary_search_2d.cpp
+++ binary_search_2d.cpp
@@ -1,22 +1,26 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-    int rows, cols;
-    
-    // Input dimensions
-    cout << "Enter number of rows: ";
-    cin >> rows;
-    cout << "Enter number of columns: ";
-    cin >> cols;
-    
-    // Create 2D array
-    int arr[rows][cols];
-    
-    // Input array elements
-    cout << "Enter array elements:\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cin >> arr[i][j];
+bool searchMatrix(vector<vector<int>> m, int target) {
+    int row=m.size();
+    int col=m[0].size();
+    int s=0;
+    int e=row*col-1;        
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        
+        // mid/col gives row of mid and mid%col gives col of mid in that matrix 
+
+        int ele=m[mid/col][mid%col]; 
+        if(ele==target){
+            return 1;
+        }
+        if(ele<target){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
         }
     }
+    return 0;
 }
